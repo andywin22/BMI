b3efa1f66b4398275c8daa5f864136c8e59f4483
Jung <andyandywin22@gmail.com>
2023-07-31 20:29:03 +0800

    Update[5] RWD(phone), check if input has value

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
 function caculate(){
     let cm = document.getElementById("cm").value;
     let kg = document.getElementById("kg").value;
-    let BMI = kg/(cm*cm);
-    
-    BMI = BMI.toFixed(2);
-    document.getElementById("BMI").value = BMI;
 
-    if (BMI < 18.5){
-        document.getElementById("ans").value = "Too skinny";}
-    else if (18.5 <= BMI && BMI < 24){
-        document.getElementById("ans").value = "Avg";}
-    else if (24 <= BMI && BMI< 27) {
-        document.getElementById("ans").value = "Above Avg a bit but no worries";}
-    else if (27 <= BMI && BMI< 30) {
-        document.getElementById("ans").value ="A little high better watch out";}
-    else if (30 <= BMI && BMI < 35){
-        document.getElementById("ans").value ="Middle obesity";}
-    else {
-        document.getElementById("ans").value ="Extreme obesity !";}
+    if (cm == null || cm =="" && kg == null || kg ==""){
+            alert("Please input your value");
     }
+
+    else{
+        let bmi = kg/(cm**2);   
+        bmi = bmi.toFixed(2);
+        document.getElementById("bmi").value = bmi;
+
+        if (bmi < 18.5){
+            document.getElementById("ans").value = "Too skinny";}
+        else if (18.5 <= bmi && bmi < 24){
+            document.getElementById("ans").value = "Avg";}
+        else if (24 <= bmi && bmi< 27) {
+            document.getElementById("ans").value = "Above Avg a bit but no worries";}
+        else if (27 <= bmi && bmi< 30) {
+            document.getElementById("ans").value = "A little high better watch out";}
+        else if (30 <= bmi && bmi < 35){
+            document.getElementById("ans").value = "Middle obesity";}
+        else if(bmi>=35) {
+            document.getElementById("ans").value = "Extreme obesity !";}
+    }
+}
